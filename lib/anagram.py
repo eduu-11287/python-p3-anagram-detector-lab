ea633e5159fd36bcd709f5c3f5b4f6ee5b4e0255
Edwin <edmboche141@gmail.com>
2025-02-18 13:11:14 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.sorted_word = sorted(word)
+    
+    def match(self, possible_anagram):
+        matches = []
+        
+        for word in possible_anagram:
+            if sorted(word) == self.sorted_word:
+                matches.append(word)
+        
+        return matches  
+
+listen = Anagram("enlist")
+results = listen.match(['listen', 'poison', 'hello'])
+print(results)  
